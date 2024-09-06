3eca6b71b67ae8251076b7ff38e4ae0772dbfeee
uhini0201 <uhinimukherjee@email.com>
2024-09-06 23:01:15 +0530

    Session ID

--- MedicAI/MedicAI_Backend/urls.py
+++ MedicAI/MedicAI_Backend/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('upload-report/',views.upload_report),
-    path('chat-interact/',views.chat_interact)
+    path('chat-interact/',views.chat_interact),
+    path('session-start/',views.session_start)
 ]
--- MedicAI/MedicAI_Backend/views.py
+++ MedicAI/MedicAI_Backend/views.py
@@ -10,8 +10,9 @@ from .ml_service import chat_service
 def upload_report(request):
     File = request.FILES['File']
     UserId = request.data['UserId']
+    SessionId=request.data['SessionId']
     
-    response = db_helper.upload_document(UserId, File)
+    response = db_helper.upload_document(UserId, File,SessionId)
 
 
     return Response(response)
@@ -21,12 +22,24 @@ def chat_interact(request):
     user_message = request.data['Message']
     UserId = request.data['UserId']
     TimeStamp = request.data['TimeStamp']
+    SessionId=request.data['SessionId']
 
     #______Code for openai_______#
     SystemMessage = chat_service.Call_OpenAI(user_message)
 
-    response = db_helper.upload_chat(UserId,user_message,SystemMessage,TimeStamp)
+    response = db_helper.upload_chat(UserId,user_message,SystemMessage,TimeStamp,SessionId)
     response.update({"SystemMessage":SystemMessage})
 
     return Response(response)
 
+
+@api_view(['POST'])
+def session_start(request):
+    SessionId=request.data['SessionId']
+    UserId = request.data['UserId']
+
+    print((SessionId,UserId))
+
+    response = db_helper.upload_sessions(SessionId,UserId)
+
+    return Response(response)
